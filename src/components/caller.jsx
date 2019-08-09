73b94b1aba76a65c281c512a5fd248392c56c270
Luis Rubio <luirting@gmail.com>
2019-08-08 23:54:37 -0700

    Added css library, change styles, add default prop.
    Added semantic ui for css library, changes styles on caller component
    and add default prop on App.

--- src/components/caller.jsx
+++ src/components/caller.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Button, Icon } from 'semantic-ui-react';
 
 const Caller = ({makeCall, hangUpCall, inCall}) => (
     <div>
         {inCall ? <p>Ya esta en llamada</p> : <p>Disponible para llamar</p>}
         <h2>Hello i am the caller</h2>
-        {inCall ? <button onClick={hangUpCall}> Terminar llamada </button>
-            : <button onClick={makeCall}> Make a call </button>}
+        {inCall ? <Button icon labelPosition='right' color='red' 
+            onClick={hangUpCall} >
+                Terminar llamada
+                <Icon name='call' />
+            </Button>
+           :<Button icon labelPosition='left' color='green' onClick={makeCall} >
+                Make a call
+                <Icon name='call' />
+            </Button>}
     </div>
 );
 
